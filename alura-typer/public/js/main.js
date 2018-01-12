91eb68e10ad472b501aa67bedcd197f58c21a94d
andersonlemos <anderson.lemos@dmorj.com.br>
2018-01-12 17:51:15 -0200

    update stylesheet

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -2,36 +2,37 @@ var campo = $('.campo-digitacao');
 var tempoInicial = $('#tempo-digitacao').text();
 
 //$(document).ready(function(){});
-$(function(){
+$(function () {
     atualizaTamanhoFrase();
     inicializaContadores();
     inicializaCronometro();
+    inicializarMarcadores();
     $("#botao-reiniciar").click(reiniciaJogo);
 })
 
-function atualizaTamanhoFrase(){
+function atualizaTamanhoFrase() {
     var frase = $('.frase').text();
     var numPalavras = frase.split(" ").length;
     var tamanhoFrase = $('#tamanho-frase');
-    tamanhoFrase.text(numPalavras);    
+    tamanhoFrase.text(numPalavras);
 }
 
-function inicializaContadores(){
+function inicializaContadores() {
     campo.on('input', function () {
         var conteudo = campo.val();
         var qtdPalavras = conteudo.split(/\S+/).length - 1;
-    
+
         $("#contador-palavras").text(qtdPalavras);
-    
+
         var conteudoSemEspaco = conteudo.replace(/\s+/g, '');
-    
+
         var qtdCaracteres = conteudoSemEspaco.length;
         $("#contador-caracteres").text(qtdCaracteres);
-    
+
     });
 }
 
-function inicializaCronometro(){
+function inicializaCronometro() {
     campo.one('focus', function () {
         var tempoRestante = $('#tempo-digitacao').text();
         var cronometroID = setInterval(
@@ -41,18 +42,38 @@ function inicializaCronometro(){
                 if (tempoRestante < 1) {
                     campo.attr("disabled", true);
                     clearInterval(cronometroID);
-                    $("#botao-reiniciar").attr("disabled", false);
+                    campo.toogleClass('campo-desativado');
                 }
             }, 1000);
+
     });
 }
 
-function reiniciaJogo(){
-    campo.attr("disabled",false);
+function reiniciaJogo() {
+    campo.attr("disabled", false);
     campo.val("");
-    $('#contador-caracteres').text("0");
-    $('#contador-palavras').text("0");
-    $('#tempo-digitacao').text(tempoInicial);
-    inicializaCronometro()
+
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+
+    inicializaCronometro();
+
+    campo.toggleClass("campo-desativado");
+
+    campo.removeClass("borda-vermelha"); //novo
+    campo.removeClass("borda-verde"); //novo
 }
 
+function inicializarMarcadores() {
+    var frase = $('.frase').text();
+    campo.on('input', function () {
+        var digitado = campo.val();
+        var digitouCorreto = frase.startsWith(digitado);
+        if( frase.startsWith(digitado)) {
+            campo.addClass("borda-verde");
+           } else {
+            campo.addClass("borda-vermelha");
+           }
+    });
+}
